364404b6e2d6fbab669f3d1be02f6580e7ce583e
Steven Dall <steven.dall@capgemini.com>
2025-12-01 22:30:03 +0100

    Day1

---
+++ Advent.Utils/FileReader.cs
@@ -0,0 +1,14 @@
+﻿namespace Advent.Utils;
+
+public class FileReader
+{
+    public static string[] ReadLines(string filePath)
+    {
+        return System.IO.File.ReadAllLines(filePath);
+    }
+    
+    public static string ReadAllText(string filePath)
+    {
+        return System.IO.File.ReadAllText(filePath);
+    }
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Advent.Utils;
+
+var lines = FileReader.ReadLines("./files/set1.txt");
+var numbers = lines.Select(GetNumberForLine);
+int midpoint = 50;
+int range = midpoint * 2;
+int start = 50;
+int counter = 0;
+
+_ = numbers.Aggregate(start, (current, number) =>
+{
+    int times = number / range;
+    counter += Math.Abs(times);
+    int next = (current + (number % range));
+    switch (next)
+    {
+        case > 99:
+            next %= range;
+            if (next != 0)
+            {
+                counter++;
+            }
+            break;
+        case < 0:
+            next = (next % range) + range;
+            if (current != 0)
+            {
+                counter++;
+            }
+
+            break;
+    }
+
+    if (next == 0)
+    {
+        counter++;
+    }
+
+    return next;
+});
+
+Console.WriteLine($"Result: {counter}");
+
+
+static int GetNumberForLine(string line)
+{
+    var direction = line[0];
+    var number = (int.Parse(line[1..]));
+    return direction == 'L' ? -number : number;
+}
